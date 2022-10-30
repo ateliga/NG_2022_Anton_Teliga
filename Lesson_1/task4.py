2db48d855715646723a75b75b284f37918a499d8
ateliga <116106070+ateliga@users.noreply.github.com>
2022-10-30 12:37:28 +0300

    Add files via upload

---
+++ Lesson_1/task4.py
@@ -0,0 +1,21 @@
+import math 
+
+print("Enter the coefficients for the equation:")
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+
+D = b**2 - 4*a*c
+
+if D < 0:    
+    print ("no roots")
+elif D == 0:    
+    x = (-b + D** .5) / (2*a)
+    print ("D = ", D)
+    print ("one root: ", x)
+else:   
+    x1 =  (-b + D** .5) / (2*a)
+    x2 =  (-b - D** .5) / (2*a)
+    print ("D = ", D)
+    print ("first root =  ", x1)
+    print ("second root =  ", x2)
