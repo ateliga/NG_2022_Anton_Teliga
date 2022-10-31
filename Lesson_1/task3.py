75917d4a070de9412761e67045a9f9f72f4cdda9
ateliga <116106070+ateliga@users.noreply.github.com>
2022-10-31 19:23:38 +0200

    Delete task3.py

--- Lesson_1/task3.py
+++
@@ -1,8 +0,0 @@
-import time
-time.time()
-print("unix time:")
-print(time.time())
-print ('-' * 20)
-print("convenient unix time:")
-timeline = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime())
-print(timeline) 
